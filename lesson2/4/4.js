05db4fae361611af4741933b7cc9c69d335067d3
rrrew <92250583+rrrew@users.noreply.github.com>
2022-02-10 13:42:13 +0300

    completed lesson 2

---
+++ lesson2/4/4.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/**
+ * Функция складывает два числа
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+
+function add(a, b) {
+    return a + b;
+}
+
+/**
+ * Функция вычитает два числа
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+
+function subtract(a, b) {
+    return a - b;
+}
+
+/**
+ * Функция умножает два числа
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+
+function multiply(a, b) {
+    return a * b;
+}
+
+/**
+ * Функция делит два числа
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+
+function divide(a, b) {
+    return a / b;
+}
